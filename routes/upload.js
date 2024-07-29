5d5eebf4550d9c513669f13b6bfbf26e12f4c75b
Priyanka Kumari <kumaripriyanka1832@gmail.com>
2024-07-29 21:24:03 +0530

    added upload

--- routes/upload.js
+++ routes/upload.js
@@ -7,53 +7,53 @@ const { AWS_ACCESS_KEY, AWS_SECRET_ACCESS_KEY } = require("../config/keys");
 
 const { v4: uuid } = require("uuid");
 const bucket = new S3Client({
-  region: "ap-south-1",
-  credentials: {
-    accessKeyId: AWS_ACCESS_KEY,
-    secretAccessKey: AWS_SECRET_ACCESS_KEY,
-  },
+    region: "ap-south-1",
+    credentials: {
+        accessKeyId: AWS_ACCESS_KEY,
+        secretAccessKey: AWS_SECRET_ACCESS_KEY,
+    },
 });
 
 const getPresignedUrl = async (fileName, fileType) => {
-  console.log(fileName, fileType);
+    console.log(fileName, fileType);
 
-  const uniqueFileName =
-    fileName.split(".")[0] + "-" + uuid() + "." + fileType.split("/")[1];
-  console.log(uniqueFileName);
+    const uniqueFileName =
+        fileName.split(".")[0] + "-" + uuid() + "." + fileType.split("/")[1];
+    console.log(uniqueFileName);
 
-  const command = new PutObjectCommand({
-    Bucket: "learnflow-app-pw",
-    Key: uniqueFileName,
-    ContentType: fileType,
-  });
+    const command = new PutObjectCommand({
+        Bucket: "learnflow-app-ps",
+        Key: uniqueFileName,
+        ContentType: fileType,
+    });
 
-  const url = await getSignedUrl(bucket, command, { expiresIn: 3600 });
-  return {
-    url,
-    fileName: uniqueFileName,
-  };
+    const url = await getSignedUrl(bucket, command, { expiresIn: 3600 });
+    return {
+        url,
+        fileName: uniqueFileName,
+    };
 };
 
 const router = new Router();
 
 router.get("/presignedUrl", ensureAuth, async (req, res) => {
-  try {
-    const { fileName, fileType } = req.query;
-    if (!fileName || !fileType) {
-      return res.status(500).send({
-        error: "Something went wrong",
-      });
+    try {
+        const { fileName, fileType } = req.query;
+        if (!fileName || !fileType) {
+            return res.status(500).send({
+                error: "Something went wrong",
+            });
+        }
+
+        const urlResponse = await getPresignedUrl(fileName, fileType);
+
+        res.status(200).send(urlResponse);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            error: "Something went wrong",
+        });
     }
-
-    const urlResponse = await getPresignedUrl(fileName, fileType);
-
-    res.status(200).send(urlResponse);
-  } catch (error) {
-    console.log(error);
-    res.status(500).send({
-      error: "Something went wrong",
-    });
-  }
 });
 
 module.exports = router;
